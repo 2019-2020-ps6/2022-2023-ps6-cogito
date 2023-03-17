d86781ca45b1b5c0acdce72b80d7261913234665
Fabien <airaudfabien@gmail.com>
2023-03-18 00:41:49 +0100

    task: Ajout composant GameQuestion

---
+++ front-end/src/app/game/question/question.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-game-question',
+    templateUrl: './question.component.html',
+    styleUrls: ['./question.component.scss']
+})
+export class GameQuestionComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void { }
+}
