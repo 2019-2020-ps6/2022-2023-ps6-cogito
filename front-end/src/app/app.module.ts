115063093fb50202fb695ea605e05918b1575d51
Fabien <airaudfabien@gmail.com>
2023-03-18 00:43:26 +0100

    task; Ajout des composants crées au site
    #4

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamePageComponent } from './game/page/page.component';
+import { GameQuestionComponent } from './game/question/question.component';
+import { GameAnswerListComponent } from './game/answer-list/answer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamePageComponent,
+    GameQuestionComponent,
+    GameAnswerListComponent
   ],
   imports: [
     BrowserModule,
