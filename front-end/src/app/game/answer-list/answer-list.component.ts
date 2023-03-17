f74855b506155a0d13b49079bf01416571e60c54
Fabien <airaudfabien@gmail.com>
2023-03-18 00:42:44 +0100

    task: Ajout composant GameAnswerList
    #4 A compléter par des composants GameAnswer

---
+++ front-end/src/app/game/answer-list/answer-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-game-answer-list',
+    templateUrl: './answer-list.component.html',
+    styleUrls: ['./answer-list.component.scss']
+})
+export class GameAnswerListComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit(): void {}
+}
