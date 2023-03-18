a76e165b0ded84404e5ae36a47065b6e72c33230
samuel bois <samuel.bois25@gmail.com>
2023-03-19 00:24:05 +0100

    feat: create a quiz #8

---
+++ front-end/src/mocks/quiz-list.mock.ts
@@ -0,0 +1,46 @@
+import { Quiz } from '../models/quiz.model';
+import { Question } from '../models/question.model';
+
+export const QUESTION_ACTOR: Question = {
+    label: 'Jean Gabin a joué dans...',
+    answers: [
+        {
+            value: 'Les tuches II',
+            isCorrect: false,
+        },
+        {
+            value: 'La grande illusion',
+            isCorrect: true,
+        }
+    ]
+};
+
+export const QUESTION_SPORT: Question = {
+    label: 'Le sport le plus populaire au monde est...',
+    answers: [
+        {
+            value: 'Le football',
+            isCorrect: true,
+        },
+        {
+            value: 'Le basketball',
+            isCorrect: false,
+        }
+    ]
+};
+
+export const QUIZ_LIST: Quiz[] = [
+    {
+        name: 'Villes françaises',
+        theme: 'Géographie',
+        questions: [],
+    },
+    {
+        name: 'Les acteurs',
+        questions: [QUESTION_ACTOR],
+    },
+    {
+        name: 'Les sports',
+        questions: [QUESTION_SPORT],
+    }
+];
---
+++ front-end/src/services/quiz.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { Quiz } from '../models/quiz.model';
+import { QUIZ_LIST } from '../mocks/quiz-list.mock';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+  /**
+   * Services Documentation:
+   * https://angular.io/docs/ts/latest/tutorial/toh-pt4.html
+   */
+
+   /**
+    * The list of quiz.
+    * The list is retrieved from the mock.
+    */
+  private quizzes: Quiz[] = QUIZ_LIST;
+  private searchTerms = new Subject<string>();
+
+  /**
+   * Observable which contains the list of the quiz.
+   * Naming convention: Add '$' at the end of the variable name to highlight it as an Observable.
+   */
+  public quizzes$: BehaviorSubject<Quiz[]> = new BehaviorSubject(QUIZ_LIST);
+
+  constructor() {
+  }
+
+  addQuiz(quiz: Quiz) {
+    
+    // You need here to update the list of quiz and then update our observable (Subject) with the new list
+    // More info: https://angular.io/tutorial/toh-pt6#the-searchterms-rxjs-subjecta nouvelle valeur de la liste des quiz
+    this.quizzes.push(quiz);
+    this.quizzes$.next(this.quizzes);
+  }
+
+  deleteQuiz(quiz: Quiz) {
+    this.quizzes.forEach((value,index)=>{
+      if(value.name == quiz.name) this.quizzes.splice(index,1);
+  });
+  this.quizzes$.next(this.quizzes);
+  }
+}
+
