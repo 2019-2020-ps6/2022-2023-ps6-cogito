d567ac2aaebe4daec7b7cb929a1c7594eac88770
Fabien <airaudfabien@gmail.com>
2023-03-27 00:50:29 +0200

    task: Adding theme list
    #23

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ThemePageComponent } from './themes/page/page.component';
+import { ThemeListComponent } from './themes/theme-list/theme-list.component';
 import { GamePageComponent } from './game/page/page.component';
 import { GameQuestionComponent } from './game/question/question.component';
 import { GameAnswerComponent } from './game/answer/answer.component';
@@ -14,6 +15,7 @@ import { GameAnswerComponent } from './game/answer/answer.component';
     AppComponent,
     HomePageComponent,
     ThemePageComponent,
+    ThemeListComponent,
     GamePageComponent,
     GameQuestionComponent,
     GameAnswerComponent
