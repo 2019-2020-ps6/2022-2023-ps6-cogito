7f9ff076ec430c5bf3801e3ec97bf14317ffbb8a
Fabien <airaudfabien@gmail.com>
2023-03-27 01:49:55 +0200

    task: Adding quizzes and links with game
    #24

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { ThemeListComponent } from './themes/theme-list/theme-list.component';
 import { ThemeComponent } from './themes/theme/theme.component';
 import { QuizPageComponent } from './quizzes/page/page.component';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
-// import { QuizComponent } from './quizzes/quiz/quiz.component';
+import { QuizComponent } from './quizzes/quiz/quiz.component';
 import { GamePageComponent } from './game/page/page.component';
 import { GameQuestionComponent } from './game/question/question.component';
 import { GameAnswerComponent } from './game/answer/answer.component';
@@ -23,6 +23,7 @@ import { GameAnswerComponent } from './game/answer/answer.component';
     ThemeComponent,
     QuizPageComponent,
     QuizListComponent,
+    QuizComponent,
     GamePageComponent,
     GameQuestionComponent,
     GameAnswerComponent
