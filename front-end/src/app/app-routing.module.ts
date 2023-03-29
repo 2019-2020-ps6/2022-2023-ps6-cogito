20ae685d7bf48e20157af3331a2b5b1ff58fa1dc
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-03-29 12:55:08 +0200

    fix: now the app-header component show the title of the route used #32

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { QuizPageComponent } from './quizzes/page/page.component';
 import { GamePageComponent } from './game/page/page.component';
 
 const routes: Routes = [
-  { path: 'home-page', component: HomePageComponent, data: {title: 'Accueil'} },
-  { path: 'theme-page', component: ThemePageComponent, data: {title: 'Themes'} },
-  { path: 'quiz-page', component: QuizPageComponent, data: {title: 'Quizzes'} },
-  { path: 'game-page', component: GamePageComponent, data: {title: 'Game'} },
+  { path: 'home-page', component: HomePageComponent, data:{title: 'Accueil'} },
+  { path: 'theme-page', component: ThemePageComponent, data : {title: 'Themes'} },
+  { path: 'quiz-page', component: QuizPageComponent, data : {title: 'Quizzes'} },
+  { path: 'game-page', component: GamePageComponent, data : {title: 'Game'} },
   { path: '', redirectTo: 'home-page', pathMatch: 'full'}
 ];
 
