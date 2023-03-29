3024e9d8d102f0ae6785cc7404f172f35a227266
Dezarnaud Julian <julian.dezarnaud@etu.unice.fr>
2023-03-29 22:09:14 +0200

    Implementations of ThemList Buttons #37

--- front-end/src/mocks/theme.mock.ts
+++ front-end/src/mocks/theme.mock.ts
@@ -1,9 +1,101 @@
 import { Theme } from "src/models/theme.model";
 import { QUIZZES_MUSIQUE } from "./quiz.mock";
 
-export const THEME_MUSIQUE: Theme = {
-    id: 1,
-    title: "Musique",
+export const THEME_MUSIQUE: Theme[] =[
+    {   
+        id: 1,
+        title: "AMusique",
+        image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+        quizList: QUIZZES_MUSIQUE
+    }, 
+    {
+        id: 1,
+    title: "BMusique",
     image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
     quizList: QUIZZES_MUSIQUE
-};
+    },
+    {   
+        id: 1,
+        title: "CMusique",
+        image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+        quizList: QUIZZES_MUSIQUE
+    }, 
+    {
+        id: 1,
+    title: "DMusique",
+    image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+    quizList: QUIZZES_MUSIQUE
+    },
+    {   
+        id: 1,
+        title: "EMusique",
+        image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+        quizList: QUIZZES_MUSIQUE
+    }, 
+    {
+        id: 1,
+    title: "FMusique",
+    image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+    quizList: QUIZZES_MUSIQUE
+    },
+    {   
+        id: 1,
+        title: "GMusique",
+        image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+        quizList: QUIZZES_MUSIQUE
+    }, 
+    {
+        id: 1,
+    title: "HMusique",
+    image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+    quizList: QUIZZES_MUSIQUE
+    },
+    {   
+        id: 1,
+        title: "IMusique",
+        image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+        quizList: QUIZZES_MUSIQUE
+    }, 
+    {
+        id: 1,
+    title: "JMusique",
+    image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+    quizList: QUIZZES_MUSIQUE
+    },
+    {   
+        id: 1,
+        title: "KMusique",
+        image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+        quizList: QUIZZES_MUSIQUE
+    }, 
+    {
+        id: 1,
+    title: "LMusique",
+    image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+    quizList: QUIZZES_MUSIQUE
+    },
+    {   
+        id: 1,
+        title: "MMusique",
+        image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+        quizList: QUIZZES_MUSIQUE
+    }, 
+    {
+        id: 1,
+    title: "NMusique",
+    image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+    quizList: QUIZZES_MUSIQUE
+    },
+    {   
+        id: 1,
+        title: "OMusique",
+        image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+        quizList: QUIZZES_MUSIQUE
+    }, 
+    {
+        id: 1,
+    title: "PMusique",
+    image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+    quizList: QUIZZES_MUSIQUE
+    }
+]
