28de2daa96da69e493916cf4c5e1a1f40e169316
Fabien <airaudfabien@gmail.com>
2023-03-30 23:09:42 +0200

    task: Mocks correction
    #38

--- front-end/src/mocks/quiz.mock.ts
+++ front-end/src/mocks/quiz.mock.ts
@@ -4,13 +4,13 @@ import { QUESTION_ECH1, QUESTIONS_CHA, QUESTIONS_INS } from "./quizQuestion.mock
 export const QUIZ_INSTRUMENTS: Quiz = {
     id: 1,
     title: "Instruments",
-    image: "https://c8.alamy.com/compfr/2beygn2/collage-pile-de-divers-instruments-de-musique-guitare-electrique-violon-clavier-piano-bongo-batterie-tamburin-harmonica-trompette-goujon-a-percussion-en-laiton-2beygn2.jpg",
+    picture: "https://c8.alamy.com/compfr/2beygn2/collage-pile-de-divers-instruments-de-musique-guitare-electrique-violon-clavier-piano-bongo-batterie-tamburin-harmonica-trompette-goujon-a-percussion-en-laiton-2beygn2.jpg",
     quizQuestionList: QUESTIONS_INS
 };
 export const QUIZ_CHANSONS: Quiz = {
     id: 2,
     title: "Chansons",
-    image: "https://media2.ledevoir.com/images_galerie/nwd_897852_715446/image.jpg",
+    picture: "https://media2.ledevoir.com/images_galerie/nwd_897852_715446/image.jpg",
     quizQuestionList: QUESTIONS_CHA
 };
 export const QUIZZES_MUSIQUE: Quiz[] = [QUIZ_INSTRUMENTS, QUIZ_CHANSONS];
@@ -19,6 +19,9 @@ export const QUIZZES_MUSIQUE: Quiz[] = [QUIZ_INSTRUMENTS, QUIZ_CHANSONS];
 export const QUIZ_ECHECS: Quiz = {
     id: 3,
     title: "Echecs",
-    image: "https://static.fnac-static.com/multimedia/Images/55/55/B4/EB/15447125-1505-1540-1/tsp20200826152348/Jeu-d-echecs-magnetique-en-bois-pliable.jpg",
+    picture: "https://static.fnac-static.com/multimedia/Images/55/55/B4/EB/15447125-1505-1540-1/tsp20200826152348/Jeu-d-echecs-magnetique-en-bois-pliable.jpg",
     quizQuestionList: [QUESTION_ECH1]
 };
+
+
+export const QUIZ_LIST: Quiz[] = [QUIZ_INSTRUMENTS, QUIZ_CHANSONS, QUIZ_ECHECS];
--- front-end/src/mocks/theme.mock.ts
+++ front-end/src/mocks/theme.mock.ts
@@ -4,12 +4,13 @@ import { QUIZ_ECHECS, QUIZZES_MUSIQUE } from "./quiz.mock";
 export const THEME_MUSIQUE: Theme = {
     id: 1,
     title: "Musique",
-    image: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
+    picture: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
     quizList: QUIZZES_MUSIQUE
 };
 export const THEME_SPORT: Theme = {
     id: 1,
     title: "Sport",
-    image: "https://img.freepik.com/vecteurs-libre/concept-equipement-sport_1284-13034.jpg?w=2000",
+    picture: "https://img.freepik.com/vecteurs-libre/concept-equipement-sport_1284-13034.jpg?w=2000",
     quizList: [QUIZ_ECHECS]
 };
+export const THEME_LIST: Theme[] = [THEME_MUSIQUE, THEME_SPORT];
