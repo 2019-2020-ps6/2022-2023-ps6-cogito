ddfe6eba86a12da831f15e9101197c89511065b8
Fabien <airaudfabien@gmail.com>
2023-03-30 23:12:01 +0200

    task: Merge routes and modules
    #35

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,19 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { HomePageComponent } from './home-page/home-page.component';
-import { ProfileListComponent } from './profiles/profile-list/profile-list.component';
-import { ThemePageComponent } from './themes/page/page.component';
-import { QuizPageComponent } from './quizzes/page/page.component';
-import { GamePageComponent } from './game/page/page.component';
+import { HomePageComponent } from "./home-page/home-page.component";
+import { ProfileListComponent } from "./profiles/profile-list/profile-list.component";
+import { QuestionListComponent } from "./questions/question-list/question-list.component";
+import { AdminQuizListComponent } from "./quizzes/admin-quiz-list/admin-quiz-list.component";
+import { EditQuizComponent } from "./quizzes/edit-quiz/edit-quiz.component";
+import { QuizFormComponent } from "./quizzes/quiz-form/quiz-form.component";
+import { ThemePageComponent } from "./themes/page/page.component";
+import { QuizPageComponent } from "./quizzes/page/page.component";
+import { GamePageComponent } from "./game/page/page.component";
 
 const routes: Routes = [
-    { path: 'home-page', component: HomePageComponent, data: { title: 'Accueil' } },
-    { path: 'profile-list', component: ProfileListComponent, data: { title: 'Liste des profils' } },
-    { path: 'theme-page', component: ThemePageComponent, data: { title: 'Liste des themes' } },
-    { path: 'quiz-page', component: QuizPageComponent, data: { title: 'Liste des quiz' } },
-    { path: 'game-page', component: GamePageComponent, data: { title: 'Question' } },
-    { path: '', redirectTo: 'home-page', pathMatch: 'full' }
+    { path: "home-page", component: HomePageComponent, data: { title: "Accueil" } },
+    { path: "profile-list", component: ProfileListComponent, data: { title: "Liste des profils" } },
+    { path: "theme-page", component: ThemePageComponent, data: { title: "Liste des thèmes" } },
+    { path: "quiz-page", component: QuizPageComponent, data: { title: "Liste des quiz" } },
+    { path: "game-page", component: GamePageComponent, data: { title: "Question" } },
+    { path: "admin-quiz-list", component: AdminQuizListComponent, data: { title: "Liste des quiz" } },
+    { path: "quiz-form", component: QuizFormComponent, data: { title: "Formulaire de création des quiz" } },
+    { path: "edit-quiz/:id", component: EditQuizComponent, data: { title: "Formulaire de modification des quiz" } },
+    { path: "question-list", component: QuestionListComponent, data: { title: "Liste des questions" } },
+    { path: "", redirectTo: "home-page", pathMatch: "full" }
 ];
 
 @NgModule({
