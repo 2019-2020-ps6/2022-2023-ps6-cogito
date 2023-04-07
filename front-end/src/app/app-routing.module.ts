51f79825a2b95c36ab78eb9bf725bfe9134ef16f
Fabien <airaudfabien@gmail.com>
2023-04-07 21:34:13 +0200

    refactor: clean files in app #52

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,33 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { HomePageComponent } from "./home-page/home-page.component";
-import { ProfileListComponent } from "./profiles/profile-list/profile-list.component";
-import { QuestionListComponent } from "./questions/question-list/question-list.component";
-import { AdminQuizListComponent } from "./quizzes/admin-quiz-list/admin-quiz-list.component";
-import { EditQuizComponent } from "./quizzes/edit-quiz/edit-quiz.component";
-import { QuizFormComponent } from "./quizzes/quiz-form/quiz-form.component";
-import { ThemePageComponent } from "./themes/page/page.component";
-import { QuizPageComponent } from "./quizzes/page/page.component";
-import { GamePageComponent } from "./game/page/page.component";
-import { GameResultComponent } from './game/gameResult/gameResult.component';
-import { QuestionFormComponent } from './questions/question-form/question-form.component';
-
 const routes: Routes = [
-    { path: "home-page", component: HomePageComponent, data: { title: "Accueil" } },
-    { path: "profile-list", component: ProfileListComponent, data: { title: "Liste des profils" } },
-    { path: "theme-page", component: ThemePageComponent, data: { title: "Liste des thèmes" } },
-    { path: "quiz-page", component: QuizPageComponent, data: { title: "Liste des quiz" } },
-    { path: "game-page", component: GamePageComponent, data: { title: "Question" } },
-    { path: "game-gameResult", component: GameResultComponent, data: { title: "Résultat quiz" } },
-    { path: "admin-quiz-list", component: AdminQuizListComponent, data: { title: "Liste des quiz" } },
-    { path: "quiz-form", component: QuizFormComponent, data: { title: "Formulaire de création des quiz" } },
-    { path: "edit-quiz/:id", component: EditQuizComponent, data: { title: "Formulaire de modification des quiz" } },
-    { path: "question-list", component: QuestionListComponent, data: { title: "Liste des questions" } },
-    { path: 'question-form/:id', component: QuestionFormComponent, data: { title: "Formulaire de modification de question" } },
-    { path: 'add-question/:quizId', component: QuestionFormComponent, data: { title: "Formulaire d'ajout de question" } },
-    { path: 'add-question', component: QuestionFormComponent, data: { title: "Formulaire d'ajout de question" } },
-    { path: "", redirectTo: "home-page", pathMatch: "full" }
+
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,64 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
-import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { HomePageComponent } from "./home-page/home-page.component";
-import { ProfileListComponent } from "./profiles/profile-list/profile-list.component";
-import { ProfileComponent } from "./profiles/profile/profile.component";
-import { ThemePageComponent } from "./themes/page/page.component";
-import { ThemeListComponent } from "./themes/theme-list/theme-list.component";
-import { ThemeComponent } from "./themes/theme/theme.component";
-import { QuizPageComponent } from "./quizzes/page/page.component";
-import { QuizListComponent } from "./quizzes/quiz-list/quiz-list.component";
-import { QuizComponent } from "./quizzes/quiz/quiz.component";
-import { GamePageComponent } from "./game/page/page.component";
-import { GameQuestionComponent } from "./game/question/question.component";
-import { GameAnswerComponent } from "./game/answer/answer.component";
-import { GameResultComponent } from './game/gameResult/gameResult.component';
-import { GameQuestionResultComponent } from './game/questionResult/questionResult.component';
-import { EditQuizComponent } from "./quizzes/edit-quiz/edit-quiz.component";
-import { QuizFormComponent } from "./quizzes/quiz-form/quiz-form.component";
-import { AdminQuizListComponent } from "./quizzes/admin-quiz-list/admin-quiz-list.component";
-import { AdminQuizComponent } from "./quizzes/admin-quiz/admin-quiz.component";
-import { QuestionFormComponent } from "./questions/question-form/question-form.component";
-import { QuestionListComponent } from "./questions/question-list/question-list.component";
-import { QuestionComponent } from "./questions/question/question.component";
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HeaderComponent,
-        HomePageComponent,
-        ProfileListComponent,
-        ProfileComponent,
-        ThemePageComponent,
-        ThemeListComponent,
-        ThemeComponent,
-        QuizPageComponent,
-        QuizListComponent,
-        QuizComponent,
-        GamePageComponent,
-        GameQuestionComponent,
-        GameAnswerComponent,
-        GameResultComponent,
-        GameQuestionResultComponent,
-        EditQuizComponent,
-        QuizFormComponent,
-        AdminQuizListComponent,
-        AdminQuizComponent,
-        QuestionFormComponent,
-        QuestionListComponent,
-        QuestionComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
-        ReactiveFormsModule,
-        AppRoutingModule,
-        FormsModule
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
