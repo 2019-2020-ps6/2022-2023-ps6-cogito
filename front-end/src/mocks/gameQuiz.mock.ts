d19c9fef57c25d7cc107ec851a165d61171e93c7
Fabien <airaudfabien@gmail.com>
2023-04-09 16:26:22 +0200

    task: Adding gameQuiz mocks #55

---
+++ front-end/src/mocks/gameQuiz.mock.ts
@@ -0,0 +1,45 @@
+import { GameQuiz } from "../models/gameQuiz.model";
+
+export const GAMEQUIZ_3_QUIZ2: GameQuiz = {
+    id: 1,
+    quizId: 2,
+    title: "Chansons",
+    picture: "https://media2.ledevoir.com/images_galerie/nwd_897852_715446/image.jpg",
+    questionList: [],
+
+    startTime: new Date("April 5, 2023 16:03:15:32"),
+    endTime: new Date("April 5, 2023 16:09:58:14")
+}
+
+export const GAMEQUIZ_4_QUIZ3: GameQuiz = {
+    id: 3,
+    quizId: 3,
+    title: "Echecs",
+    picture: "https://static.fnac-static.com/multimedia/Images/55/55/B4/EB/15447125-1505-1540-1/tsp20200826152348/Jeu-d-echecs-magnetique-en-bois-pliable.jpg",
+    questionList: [],
+
+    startTime: new Date("April 7, 2023 9:40:03:01"),
+    endTime: new Date("April 7, 2023 10:00:03:01")
+}
+
+export const GAMEQUIZ_5_QUIZ1: GameQuiz = {
+    id: 3,
+    quizId: 1,
+    title: "Instruments",
+    picture: "https://c8.alamy.com/compfr/2beygn2/collage-pile-de-divers-instruments-de-musique-guitare-electrique-violon-clavier-piano-bongo-batterie-tamburin-harmonica-trompette-goujon-a-percussion-en-laiton-2beygn2.jpg",
+    questionList: [],
+
+    startTime: new Date("April 8, 2023 10:01:10:01"),
+    endTime: new Date("April 8, 2023 10:41:27:43")
+}
+
+export const GAMEQUIZ_3_QUIZ3: GameQuiz = {
+    id: 4,
+    quizId: 3,
+    title: "Echecs",
+    picture: "https://static.fnac-static.com/multimedia/Images/55/55/B4/EB/15447125-1505-1540-1/tsp20200826152348/Jeu-d-echecs-magnetique-en-bois-pliable.jpg",
+    questionList: [],
+
+    startTime: new Date("April 9, 2023 14:15:16:17"),
+    endTime: new Date("April 9, 2023 14:24:51:15")
+}
