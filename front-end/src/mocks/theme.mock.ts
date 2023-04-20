10af97a51a9cc8e3e3513d3fee57d4a417e08823
Fabien <airaudfabien@gmail.com>
2023-04-20 18:18:17 +0200

    task: Correcting theme mock #65

--- front-end/src/mocks/theme.mock.ts
+++ front-end/src/mocks/theme.mock.ts
@@ -1,17 +1,18 @@
 import { Theme } from "../models/theme.model";
+import { QUIZ_ECHECS, QUIZZES_MUSIQUE, QUIZZES_SPORT } from "./quiz.mock";
 
 export const THEME_MUSIQUE: Theme = {
     id: 1,
     title: "Musique",
     picture: "https://www.sopadec.com/images/produits/70458-01-60c34c16108ac699531493.jpg",
-    quizList: []
+    quizList: QUIZZES_MUSIQUE
 };
 
 export const THEME_SPORT: Theme = {
     id: 2,
     title: "Sport",
     picture: "https://img.freepik.com/vecteurs-libre/concept-equipement-sport_1284-13034.jpg?w=2000",
-    quizList: []
+    quizList: QUIZZES_SPORT
 };
 
 export const THEME_LIST: Theme[] = [THEME_MUSIQUE, THEME_SPORT];
