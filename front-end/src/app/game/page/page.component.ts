20e7430d8d2cdb5d652959035fbd92ee1ee22b4e
Fabien <airaudfabien@gmail.com>
2023-04-23 17:06:36 +0200

    task: Add simple display for a question #66

--- front-end/src/app/game/page/page.component.ts
+++ front-end/src/app/game/page/page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { GameQuestion } from "src/models/gameQuestion.model";
+import { GameService } from "src/services/game.service";
 
 @Component({
     selector: "app-game-page",
@@ -6,5 +8,11 @@ import { Component } from "@angular/core";
     styleUrls: ["./page.component.scss"]
 })
 export class GamePageComponent {
+    public question?: GameQuestion;
 
+    constructor(private gameService: GameService) {
+        this.gameService.currentQuestion$.subscribe((question?: GameQuestion): void => {
+            this.question = question;
+        })
+    }
 }
