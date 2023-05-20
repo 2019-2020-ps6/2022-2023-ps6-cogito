f0af64d149c256627f85ba6a71e9cee92fec5332
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-05-20 15:06:02 +0200

    feature: update quiz.model #103

--- backend/app/models/quiz.model.js
+++ backend/app/models/quiz.model.js
@@ -2,6 +2,8 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Quiz', {
-  theme: Joi.string().required(),
-  name: Joi.string().required(),
+  id: Joi.number().required(),
+  title: Joi.string().required(),
+  picture: Joi.string(),
+  questionList: Joi.array().items(Joi.number()).required(),
 })
