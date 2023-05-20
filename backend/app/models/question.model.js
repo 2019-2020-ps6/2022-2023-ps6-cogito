8fae980c7814c4680839494d26228957a0e48d7b
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-05-20 15:11:14 +0200

    feature: update question.model #103

--- backend/app/models/question.model.js
+++ backend/app/models/question.model.js
@@ -2,7 +2,13 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Question', {
-  label: Joi.string().required(),
-  quizId: Joi.number(),
-  answers: Joi.array(),
+  id: Joi.number().required(),
+  title: Joi.string().required(),
+  answersList: Joi.array().items(Joi.number()).required(),
+  defautlMediaType: Joi.number().required(), // id of the media
+  defaultAnswersMediaType: Joi.number().required(), // id of the anzwers media
+  correcting: Joi.string(),
+  hint: Joi.string(),
+  picture: Joi.string(),
+  sound: Joi.string(),
 })
