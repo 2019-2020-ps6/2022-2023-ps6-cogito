09227bbc36ef404adfcf849e63230c0a21a6ac12
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-05-20 15:12:40 +0200

    feature: update answer.model #103

--- backend/app/models/answer.model.js
+++ backend/app/models/answer.model.js
@@ -2,8 +2,9 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Answer', {
-  type: Joi.string(),
+  id: Joi.number().required(),
   value: Joi.string().required(),
   isCorrect: Joi.boolean().required(),
-  questionId: Joi.number(),
+  picture: Joi.string(),
+  sound: Joi.string(),
 })
