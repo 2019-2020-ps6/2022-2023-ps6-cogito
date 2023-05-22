0d5316768d709308a2a78a273ea4ab99b69fd7fb
Fabien <airaudfabien@gmail.com>
2023-05-22 10:22:53 +0200

    task: delete user in backend (not for our project) #107

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,13 +1,11 @@
 const Answer = require('./answer.model.js')
 const Question = require('./question.model.js')
 const Quiz = require('./quiz.model.js')
-const User = require('./user.model.js')
 const Patient = require('./patient.model.js')
 
 module.exports = {
   Answer,
   Question,
   Quiz,
-  User,
   Patient,
 }
