25cec401686776577168a1de747b99842c5ffd58
Fabien <airaudfabien@gmail.com>
2023-05-22 09:14:57 +0200

    task: Create patient's model in backend #107

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -2,10 +2,12 @@ const Answer = require('./answer.model.js')
 const Question = require('./question.model.js')
 const Quiz = require('./quiz.model.js')
 const User = require('./user.model.js')
+const Patient = require('./patient.model.js')
 
 module.exports = {
   Answer,
   Question,
   Quiz,
   User,
+  Patient,
 }
