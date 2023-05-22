7d9aab9cb8f909a5116a175a792abc50f3c74c19
Fabien <airaudfabien@gmail.com>
2023-05-22 10:02:31 +0200

    task: Add http fonctions for patients #107
    get patients, get patient by id, post patient, put patient by id and delete patient by id

--- backend/app/api/patients/index.js
+++ backend/app/api/patients/index.js
@@ -13,4 +13,38 @@ router.get('/', (req, res) => {
   }
 })
 
+router.get('/:patientId', (req, res) => {
+  try {
+    res.status(200).json(Patient.getById(req.params.patientId))
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.post('/', (req, res) => {
+  try {
+    const patient = Patient.create({ ...req.body })
+    res.status(201).json(patient)
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.put('/:patientId', (req, res) => {
+  try {
+    res.status(200).json(Patient.update(req.params.patientId, req.body))
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.delete('/:patientId', (req, res) => {
+  try {
+    Patient.delete(req.params.patientId) // peut-être renvoyer la nouvelle liste des patients
+    res.status(204).end()
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
 module.exports = router
