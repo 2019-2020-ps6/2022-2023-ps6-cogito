4ca75da0d8a13e7e8187111d588d32ba58bc9743
Fabien <airaudfabien@gmail.com>
2023-05-22 09:17:54 +0200

    task: Add patient route #107

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const QuizzesRouter = require('./quizzes')
 const UserRouter = require('./users')
+const PatientRouter = require('./patients')
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
 router.use('/quizzes', QuizzesRouter)
 router.use('/users', UserRouter)
+router.use('/patients', PatientRouter)
 
 module.exports = router
