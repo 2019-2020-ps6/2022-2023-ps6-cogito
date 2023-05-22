f15d5e89183c060f0a662ac08fbd22fcb3680f6e
samuel bois <samuel.bois25@gmail.com>
2023-05-22 10:31:46 +0400

    feat: configuration model

---
+++ backend/app/models/configuration.model.js
@@ -0,0 +1,29 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('Configuration', {
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+
+    fontFamily: Joi.string().required(),
+    fontSize: Joi.number().required(),
+
+    pictures: Joi.boolean().required(),
+    sounds: Joi.boolean().required(),
+    multipleAnswers: Joi.boolean().required(),
+    hints: Joi.boolean().required(),
+    timeDisplayHint: Joi.number(),
+    difficulty: Joi.number().required(),
+    againFalseQuestion: Joi.boolean().required(),
+
+    correctAnswerWindow: Joi.boolean().required(),
+    correctDescription: Joi.boolean().required(),
+    correctPicture: Joi.boolean().required(),
+    correctSound: Joi.boolean().required(),
+
+    wrongAnswerWindow: Joi.boolean().required(),
+    wrongDescription: Joi.boolean().required(),
+    wrongPicture: Joi.boolean().required(),
+    wrongSound: Joi.boolean().required(),
+})
