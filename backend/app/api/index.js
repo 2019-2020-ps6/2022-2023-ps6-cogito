830f462a3322e30ef7dbe12a2ca7814206528be5
Dezarnaud Julian <julian.dezarnaud@etu.unice.fr>
2023-05-22 10:38:42 +0200

    bug fix #103

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const ThemesRouter = require('./themes')
+const QuizzesRouter = require('./quizzes')
 const UserRouter = require('./users')
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
 router.use('/themes', ThemesRouter)
 router.use('/users', UserRouter)
+router.use('/quizzes', QuizzesRouter)
 
 module.exports = router
--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,5 +1,6 @@
 const Answer = require('./answer.model.js')
 const Question = require('./question.model.js')
+
 const Quiz = require('./quiz.model.js')
 const User = require('./user.model.js')
 const Theme = require('./theme.model.js')
