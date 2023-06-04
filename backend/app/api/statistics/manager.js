aa65a3d1ff4e6f99bf2be086f1cdc62a0c160b7e
Fabien <airaudfabien@gmail.com>
2023-06-04 17:08:42 +0200

    task: Remove console log #115

--- backend/app/api/statistics/manager.js
+++ backend/app/api/statistics/manager.js
@@ -3,7 +3,6 @@ const Statistic = require('../../models/statistic.model')
 const findStatisticByPatientId = (patientId) => {
   const statistics = Statistic.get()
   const index = statistics.findIndex((value) => value.patientId.toString() === patientId)
-  console.log(index)
   return statistics[index]
 }
 
