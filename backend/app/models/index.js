01e13775e244a512f56bf33bf7e5a4a20be6e6c9
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-06-04 22:17:33 +0200

    fix: back merging errors #124

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,7 +1,5 @@
-const Answer = require('./answer.model.js')
 const Question = require('./question.model.js')
 const Quiz = require('./quiz.model.js')
-const User = require('./user.model.js')
 const Configuration = require('./configuration.model.js')
 const Theme = require('./theme.model.js')
 const Patient = require('./patient.model')
@@ -10,11 +8,9 @@ const GameQuiz = require('./gameQuiz.model')
 
 module.exports = {
   Theme,
-  User,
   Configuration,
   Quiz,
   Question,
-  Answer,
   Patient,
   Statistic,
   GameQuiz,
