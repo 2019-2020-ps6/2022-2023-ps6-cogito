3576e0c307ba660676f7962d6ed954b0b2f018bc
Fabien <airaudfabien@gmail.com>
2023-06-04 17:08:10 +0200

    task: Remove unused comments #115

--- backend/app/api/statistics/manager.js
+++ backend/app/api/statistics/manager.js
@@ -2,11 +2,6 @@ const Statistic = require('../../models/statistic.model')
 
 const findStatisticByPatientId = (patientId) => {
   const statistics = Statistic.get()
-  /* for (const statistic of statistics) {
-    if (statistic.patientId.toString() === patientId) {
-      return statistic
-    }
-  } */
   const index = statistics.findIndex((value) => value.patientId.toString() === patientId)
   console.log(index)
   return statistics[index]
