7f7e34201e3b99f613cb2d6eeedb4cc3f1b9130e
Fabien <airaudfabien@gmail.com>
2023-06-06 01:28:20 +0200

    task: Add get requests #126

--- backend/app/api/gameQuiz/gameQuestions/index.js
+++ backend/app/api/gameQuiz/gameQuestions/index.js
@@ -2,13 +2,38 @@ const { Router } = require('express')
 
 const manageAllErrors = require('../../../utils/routes/error-management')
 const { GameQuestion } = require('../../../models')
-const { buildGameQuestion, getAllGameQuestion } = require('./manager')
+const { buildGameQuestion, getAllGameQuestion, findGameQuestionByGameQuiz, findGameQuestionByGameQuizAndQuestion } = require('./manager')
 
 const router = new Router({ mergeParams: true })
 
 router.get('/', (req, res) => {
   try {
     res.status(200).json(getAllGameQuestion())
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.get('/:gameQuestionId', (req, res) => {
+  try {
+    const gameQuestion = GameQuestion.getById(req.params.gameQuestionId)
+    res.status(200).json(buildGameQuestion(gameQuestion))
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.get('/gameQuiz/:gameQuizId', (req, res) => {
+  try {
+    res.status(200).json(findGameQuestionByGameQuiz(req.params.gameQuizId))
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.get('/gameQuiz/:gameQuizId/question/:questionId', (req, res) => {
+  try {
+    res.status(200).json(findGameQuestionByGameQuizAndQuestion(req.params.gameQuizId, req.params.questionId))
   } catch (err) {
     manageAllErrors(res, err)
   }
--- backend/app/api/gameQuiz/gameQuestions/manager.js
+++ backend/app/api/gameQuiz/gameQuestions/manager.js
@@ -7,7 +7,21 @@ const getAllGameQuestion = () => {
   return gameQuestionList.map((gameQuestion) => buildGameQuestion(gameQuestion))
 }
 
+const findGameQuestionByGameQuiz = (gameQuizId) => {
+  const gameQuestionList = GameQuestion.get()
+  const gameQuestions = gameQuestionList.filter((gameQuestion) => gameQuestion.gameQuizId.toString() === gameQuizId)
+  return gameQuestions.map((gameQuestion) => buildGameQuestion(gameQuestion))
+}
+
+const findGameQuestionByGameQuizAndQuestion = (gameQuizId, questionId) => {
+  const gameQuestionList = GameQuestion.get()
+  const gameQuestions = gameQuestionList.filter((gameQuestion) => gameQuestion.gameQuizId.toString() === gameQuizId && gameQuestion.questionId.toString() === questionId)
+  return gameQuestions.map((gameQuestion) => buildGameQuestion(gameQuestion))
+}
+
 module.exports = {
   buildGameQuestion,
   getAllGameQuestion,
+  findGameQuestionByGameQuiz,
+  findGameQuestionByGameQuizAndQuestion,
 }
