07c2278f3bd0af44aa39e71f927581305f44c766
Fabien <airaudfabien@gmail.com>
2023-06-06 01:15:24 +0200

    task: Add post request #126

--- backend/app/api/gameQuiz/gameQuestions/index.js
+++ backend/app/api/gameQuiz/gameQuestions/index.js
@@ -2,13 +2,22 @@ const { Router } = require('express')
 
 const manageAllErrors = require('../../../utils/routes/error-management')
 const { GameQuestion } = require('../../../models')
-const { getAllGameQuestion } = require('./manager')
+const { buildGameQuestion, getAllGameQuestion } = require('./manager')
 
 const router = new Router({ mergeParams: true })
 
 router.get('/', (req, res) => {
   try {
     res.status(200).json(getAllGameQuestion())
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.post('/', (req, res) => {
+  try {
+    const gameQuestion = GameQuestion.create({ ...req.body })
+    res.status(201).json(buildGameQuestion(gameQuestion))
   } catch (err) {
     manageAllErrors(res, err)
   }
--- backend/app/api/gameQuiz/gameQuestions/manager.js
+++ backend/app/api/gameQuiz/gameQuestions/manager.js
@@ -3,8 +3,8 @@ const GameQuestion = require('../../../models/gameQuestion.model')
 const buildGameQuestion = (gameQuestion) => ({ ...gameQuestion, answerList: [0] })
 
 const getAllGameQuestion = () => {
-  const gameQuestions = GameQuestion.get()
-  return gameQuestions.map((gameQuestion) => buildGameQuestion(gameQuestion))
+  const gameQuestionList = GameQuestion.get()
+  return gameQuestionList.map((gameQuestion) => buildGameQuestion(gameQuestion))
 }
 
 module.exports = {
