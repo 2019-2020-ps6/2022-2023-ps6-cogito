0fbc9a57762fe83cbe28cf1d5ed36f4a996f92cb
Fabien <airaudfabien@gmail.com>
2023-06-06 10:07:35 +0200

    task: Correct patient mock and creationpatient #116

--- front-end/src/app/manager/creation_patient/page.component.ts
+++ front-end/src/app/manager/creation_patient/page.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
 import { Patient } from "src/models/patient.model";
-import { CONFIG_DEFAULT_3 } from "../../../mocks/configuration.mock";
+import { CONFIG_DEFAULT_3, CONFIG_DEFAULT_4, CONFIG_DEFAULT_5 } from "../../../mocks/configuration.mock";
+import { myMap, myMap2 } from "../../../mocks/patient.mock";
+import { STAT_GOOD_3, STAT_GOOD_4, STAT_GOOD_5 } from "../../../mocks/statistics.mock";
 import { Configuration } from "../../../models/configuration.model";
 import { Statistics } from "../../../models/statistics.model";
 import { PatientPageListComponent } from "../profil_list/page.component";
@@ -61,7 +63,19 @@ export class CreationPatientComponent implements OnInit {
             console.log("Question is not valid");
         }
         else {
-            this.patientService.addPatient(this.patient as Patient);
+            if (this.patient.stage === 3) {
+                this.patient.statistics = STAT_GOOD_3;
+                this.patient.configuration = CONFIG_DEFAULT_3;
+            } else if (this.patient.stage === 4) {
+                this.patient.statistics = STAT_GOOD_4;
+                this.patient.configuration = CONFIG_DEFAULT_4;
+            } else if (this.patient.stage === 5) {
+                this.patient.statistics = STAT_GOOD_5;
+                this.patient.configuration = CONFIG_DEFAULT_5;
+            }
+            this.patient.quizResult = myMap
+            this.patient.questionResult = myMap2
+            this.patientService.addPatient(this.patient);
         }
     }
 
