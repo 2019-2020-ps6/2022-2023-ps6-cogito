2f31d1f0dbf05d9a2f7a0a2a8e75c12c5b651f5b
Fabien <airaudfabien@gmail.com>
2023-06-10 22:52:48 +0200

    task: Correct question route #108

--- backend/app/api/quizzes/questions/index.js
+++ backend/app/api/quizzes/questions/index.js
@@ -2,7 +2,13 @@ const { Router } = require('express')
 
 const { Question } = require('../../../models')
 const manageAllErrors = require('../../../utils/routes/error-management')
-const { checkQuestion, buildQuestion, buildQuestions, findQuizQuestions } = require('./manager')
+const {
+  buildQuestion,
+  buildQuestions,
+  findQuizQuestions,
+  createQuestion,
+  updateQuestion,
+} = require('./manager')
 
 const router = new Router({ mergeParams: true })
 
@@ -33,9 +39,7 @@ router.get('/quiz/:quizId', (req, res) => {
 
 router.post('/', (req, res) => {
   try {
-    checkQuestion({ ...req.body })
-    const question = Question.create({ ...req.body })
-    res.status(201).json(buildQuestion(question))
+    res.status(201).json(createQuestion({ ...req.body }))
   } catch (err) {
     manageAllErrors(res, err)
   }
@@ -43,9 +47,7 @@ router.post('/', (req, res) => {
 
 router.put('/:questionId', (req, res) => {
   try {
-    checkQuestion({ ...req.body })
-    const question = Question.update(req.params.questionId, req.body)
-    res.status(200).json(buildQuestion(question))
+    res.status(200).json(updateQuestion(req.params.questionId, req.body))
   } catch (err) {
     manageAllErrors(res, err)
   }
--- backend/app/api/quizzes/questions/manager.js
+++ backend/app/api/quizzes/questions/manager.js
@@ -1,13 +1,8 @@
 const { Question, Quiz } = require('../../../models')
 const { findQuestionAnswers } = require('./answers/manager')
 
-const checkQuestion = (question) => {
-  const { quizId } = question
-  if (quizId) Quiz.getById(quizId)
-}
-
 const buildQuestion = (question) => {
-  const answerList = findQuestionAnswers(question.id.toString())
+  const answerList = findQuestionAnswers(question.id)
   return { ...question, answerList }
 }
 
@@ -17,14 +12,37 @@ const buildQuestions = () => {
 }
 
 const findQuizQuestions = (quizId) => {
+  // Check parameters type
+  const newQuizId = (typeof quizId === 'string') ? parseInt(quizId, 10) : quizId
+
   const questionList = Question.get()
-  const questions = questionList.filter((question) => question.quizId.toString() === quizId)
+  const questions = questionList.filter((question) => question.quizId === newQuizId)
   return questions.map((question) => buildQuestion(question))
 }
 
+const checkQuestion = (question) => {
+  // Check if existing properties are correct
+  if (question.quizId) Quiz.getById(question.quizId)
+
+  // Remove properties that are not in backend Question
+  const { answerList, correcting, ...newQuestion } = question
+  return newQuestion
+}
+
+const createQuestion = (question) => {
+  const newQuestion = checkQuestion(question)
+  return buildQuestion(Question.create(newQuestion))
+}
+
+const updateQuestion = (questionId, question) => {
+  const newQuestion = checkQuestion(question)
+  return buildQuestion(Question.update(questionId, newQuestion))
+}
+
 module.exports = {
-  checkQuestion,
   buildQuestion,
   buildQuestions,
   findQuizQuestions,
+  createQuestion,
+  updateQuestion,
 }
