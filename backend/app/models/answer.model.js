c2caace9c163ae25ac0083851284f643a6e7c667
Fabien <airaudfabien@gmail.com>
2023-06-10 15:26:14 +0200

    task: Add questionId #139

--- backend/app/models/answer.model.js
+++ backend/app/models/answer.model.js
@@ -3,6 +3,7 @@ const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Answer', {
   id: Joi.number().required(),
+  questionId: Joi.number().required(),
   value: Joi.string().min(1).required(),
   isCorrect: Joi.boolean().required(),
   picture: Joi.string().min(2),
