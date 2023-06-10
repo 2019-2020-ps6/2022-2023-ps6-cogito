ea8db0f5d9ecf40529e8e11e69c1f88e682ea2e0
Fabien <airaudfabien@gmail.com>
2023-06-10 14:18:29 +0200

    task: Correct functions #138

--- backend/app/api/quizzes/questions/index.js
+++ backend/app/api/quizzes/questions/index.js
@@ -1,88 +1,61 @@
 const { Router } = require('express')
 
-const { Answer, Quiz, Question } = require('../../../models')
+const { Question } = require('../../../models')
 const manageAllErrors = require('../../../utils/routes/error-management')
-const AnswersRouter = require('./answers')
-const { filterQuestionsFromQuizz, getQuestionFromQuiz } = require('./manager')
+const { buildQuestion, buildQuestions, findQuizQuestions } = require('./manager')
 
 const router = new Router({ mergeParams: true })
 
+router.get('/', (req, res) => {
+  try {
+    res.status(200).json(buildQuestions())
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
 
-/**
- * Get all questions from a quiz
- */
+router.get('/:questionId', (req, res) => {
+  try {
+    const question = Question.getById(req.params.questionId)
+    res.status(200).json(buildQuestion(question))
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
 
 router.get('/quiz/:quizId', (req, res) => {
-  console.log(req.params)
   try {
-    // Check if quizId exists, if not it will throw a NotFoundError
-    Quiz.getById(req.params.quizId)
-    res.status(200).json(filterQuestionsFromQuizz(req.params.quizId))
+    res.status(200).json(findQuizQuestions(req.params.quizId))
   } catch (err) {
-    console.log(err);
     manageAllErrors(res, err)
   }
 })
 
-/**
- * Get one question with id
- */
-router.get('/question/:questionId', (req, res) => {
+router.post('/', (req, res) => {
   try {
-    const question = Question.getById(req.params.questionId)
-    res.status(200).json(question)
+    const question = Question.create({ ...req.body })
+    res.status(201).json(buildQuestion(question))
   } catch (err) {
-    console.log(err)
     manageAllErrors(res, err)
   }
 })
 
-/**
- * Create a question for a quiz
- */
-router.post('/quiz/:quizId', (req, res) => {
-  console.log(req.params)
+router.put('/:questionId', (req, res) => {
   try {
-    const quiz = Quiz.getById(req.params.quizId)
-    
-    let question = Question.create({ ...req.body })
-    quiz.questionList.push(question.id)
-    Quiz.update(quiz.id,quiz)
-    res.status(201).json(quiz)
+    const question = Question.update(req.params.questionId, req.body)
+    res.status(200).json(buildQuestion(question))
   } catch (err) {
-    console.log(err)
     manageAllErrors(res, err)
   }
 })
 
-/**
- * Update a question
- */
-router.put('/question/:questionId', (req, res) => {
+router.delete('/:questionId', (req, res) => {
   try {
-    const question = Question.getById(req.params.questionId)
-    Question.update(req.params.questionId, req.body)
-    res.status(200).json(question)
-  } catch (err) {
-    console.log(err);
-    manageAllErrors(res, err)
-  }
-})
-
-/**
- * Delete a question
- */
-router.delete('/question/:questionId', (req, res) => {
-  try {
-    // Check if the question id exists & if the question has the same quizId as the one provided in the url.
-    getQuestionFromQuiz(req.params.quizId, req.params.questionId)
     Question.delete(req.params.questionId)
     res.status(204).end()
   } catch (err) {
     manageAllErrors(res, err)
   }
 })
-
-router.use('/:questionId/answers', AnswersRouter)
 
 module.exports = router
--- backend/app/api/quizzes/questions/manager.js
+++ backend/app/api/quizzes/questions/manager.js
@@ -1,43 +1,20 @@
-const NotFoundError = require('../../../utils/errors/not-found-error.js')
-const { Quiz, Question} = require('../../../models')
+const { Question } = require('../../../models')
 
+const buildQuestion = (question) => ({ ...question, answerList: [0] })
 
-/**
- * Questions Manager.
- * This file contains all the logic needed to by the question routes.
- */
-
-/**
- * filterQuestionsFromQuizz.
- * This function filters among the questions to return only the question linked with the given quizId.
- * @param quizId
- */
-const filterQuestionsFromQuizz = (quizId) => {
-  const parsedId = parseInt(quizId, 10)
-  const quiz = Quiz.getById(parsedId)
+const buildQuestions = () => {
   const questions = Question.get()
-  console.log(questions)
-  const filteredQuestions = questions.filter((question) => quiz.questionList.includes(question.id))
-  return filteredQuestions
+  return questions.map((question) => buildQuestion(question))
 }
 
-/**
- * getQuestionFromQuiz.
- * This function retrieves a question from a quiz. It will throw a not found exception if the quizId in the question is different from the one provided in parameter.
- * @param quizId
- * @param questionId
- */
-const getQuestionFromQuiz = (quizId, questionId) => {
-  // Check if quizId exists, if not it will throw a NotFoundError
-  const quiz = Quiz.getById(quizId)
-  const quizIdInt = parseInt(quizId, 10)
-  const question = Question.getById(questionId)
-  if (question.quizId !== quizIdInt) throw new NotFoundError(`${question.name} id=${questionId} was not found for ${quiz.name} id=${quiz.id} : not found`)
-  return question
+const findQuizQuestions = (quizId) => {
+  const questionList = Question.get()
+  const questions = questionList.filter((question) => question.quizId.toString() === quizId)
+  return questions.map((question) => buildQuestion(question))
 }
-
 
 module.exports = {
-  filterQuestionsFromQuizz,
-  getQuestionFromQuiz,
+  buildQuestion,
+  buildQuestions,
+  findQuizQuestions,
 }
