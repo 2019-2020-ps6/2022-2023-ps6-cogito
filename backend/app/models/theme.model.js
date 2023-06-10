4f4874ee5a3a6eb189f5ca5a178538a996b99f36
Fabien <airaudfabien@gmail.com>
2023-06-10 11:15:41 +0200

    task: Correct theme model #136

--- backend/app/models/theme.model.js
+++ backend/app/models/theme.model.js
@@ -5,5 +5,4 @@ module.exports = new BaseModel('Theme', {
   id: Joi.number().required(),
   title: Joi.string().required(),
   picture: Joi.string(),
-  quizzesList: Joi.array().items(Joi.number()).required(),
 })
