b1c8555e3b247bdb634730b4db1cd0e65ed584b4
Fabien <airaudfabien@gmail.com>
2023-06-10 15:17:04 +0200

    task: Create answer model #139

---
+++ backend/app/models/answer.model.js
@@ -0,0 +1,10 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('Answer', {
+  id: Joi.number().required(),
+  value: Joi.string().min(1).required(),
+  isCorrect: Joi.boolean().required(),
+  picture: Joi.string().min(2),
+  sound: Joi.string().min(2),
+})
