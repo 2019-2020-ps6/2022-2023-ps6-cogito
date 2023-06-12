573098ed7ec6aa73d28f3eb8fc2ceaefae72ab0d
samuel bois <samuel.bois25@gmail.com>
2023-06-12 11:20:00 +0200

    feat: create patient fixture

---
+++ front-end/src/app/manager/creation_patient/create-patient.fixture.ts
@@ -0,0 +1,32 @@
+import { E2EComponentFixture } from "e2e/e2e-component.fixture";
+
+export class CreatePatientFixture extends E2EComponentFixture {
+    getLabelInput() {
+        return this.page.waitForSelector('app-question-form input[id="patientName"]');
+    }
+
+    getAddButton() {
+        return this.page.getByRole('button', { name: 'Ajouter' });
+    }
+
+    getCreateButton() {
+        return this.page.getByRole('button', { name: 'Create' });
+    }
+
+
+
+    /**
+    getAllAnswersInputs(type: string) {
+        const selector = `app-question-form .answer-form input[type="${type}"]`;
+        return this.page.$$(selector);
+    }
+
+
+
+    */
+
+
+    clickCreateButton(numberOfClick = 1) {
+        return this.getCreateButton().click({ clickCount: numberOfClick });
+    }
+}
