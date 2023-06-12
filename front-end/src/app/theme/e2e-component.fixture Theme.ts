d000c2e42484c2bb289f9e2a764f849789dafdd0
Dezarnaud Julian <julian.dezarnaud@etu.unice.fr>
2023-06-12 17:07:07 +0200

    improve theme fixture #149

--- front-end/src/app/theme/e2e-component.fixture Theme.ts
+++ front-end/src/app/theme/e2e-component.fixture Theme.ts
@@ -5,4 +5,32 @@ export class CreateThemeFixture extends E2EComponentFixture {
     clickCommencer() {
         return this.page.getByRole('button', { name: 'COMMENCER' }).click();
     }
+
+    clickErgo() {
+        return this.page.click('figure#adminAccount');
+    }
+
+    clickquizAndTheme() {
+        return this.page.click('p#quizAndTheme');
+    }
+
+    clickaddObject() {
+        return this.page.click('a#addObject');
+    }
+
+    clicksaveElement() {
+        return this.page.click('button#saveElement');
+    }
+
+    clickcancelElement() {
+        return this.page.click('button#annulerElement');
+    }
+
+    clicksupprElement() {
+        return this.page.click('a#supprObject');
+    }
+
+    fillNameTheme() {
+        return this.page.fill('input#nameTheme', 'test');
+    }
 }
