5ee2445876ccd90c09b68ae8a17153435fbfa4cc
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-06-13 09:34:53 +0200

    refactor: post question route #105

--- backend/app/api/quizzes/questions/index.js
+++ backend/app/api/quizzes/questions/index.js
@@ -2,6 +2,8 @@ const { Router } = require('express')
 
 const { Question } = require('../../../models')
 const manageAllErrors = require('../../../utils/routes/error-management')
+const { createAnswer } = require('./answers/manager')
+const { createCorrecting } = require('./correctings/manager')
 const {
   buildQuestion,
   buildQuestions,
@@ -37,9 +39,42 @@ router.get('/quiz/:quizId', (req, res) => {
   }
 })
 
+/***
+ * The front just has to send an object with all the requirements (question's body, correcting, answers) and a quizId
+ */
+
 router.post('/', (req, res) => {
   try {
-    res.status(201).json(createQuestion({ ...req.body }))
+    const question = {
+      quizId: req.body.quizId,
+      title: req.body.title,
+      difficulty: req.body.difficulty,
+      defaultMediaType: req.body.defaultMediaType,
+      defaultAnswersMediaType: req.body.defaultAnswersMediaType,
+      hint: req.body.hint,
+      picture: req.body.picture,
+      sound: req.body.sound,
+    };
+    const resQuestion = createQuestion(question);
+    const answers = req.body.answers.map((answer) => {
+      answer.questionId = resQuestion.id;
+      return answer;
+    });
+    const resAnswers = [];
+
+    answers.forEach(answer => {
+      console.log(answer)
+      resAnswers.push(createAnswer(answer));
+    });
+
+    resQuestion.answerList = resAnswers;
+    if(req.body.correcting){
+      const correcting = req.body.correcting;
+      correcting.questionId = resQuestion.id
+      const resCorrecting = createCorrecting(correcting);
+      resQuestion.correcting = resCorrecting;
+    }
+    res.status(201).json(resQuestion);
   } catch (err) {
     manageAllErrors(res, err)
   }
