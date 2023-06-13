1842f58e0a329c396753fa1b76a931031feb1430
samuel bois <samuel.bois25@gmail.com>
2023-06-13 11:20:10 +0200

    feat: config test go to the creation #156

--- front-end/e2e/scenarios/create-config.spec.ts
+++ front-end/e2e/scenarios/create-config.spec.ts
@@ -1,8 +1,9 @@
 import { createEnvironmentInjector } from '@angular/core';
 import { test, expect } from '@playwright/test';
-import { createPatientUrl, profilListUrl } from 'e2e/e2e.config';
+import { createPatientUrl, profilListUrl, listConfigUrl } from 'e2e/e2e.config';
 import { CreatePatientFixture } from 'src/app/manager/creation_patient/create-patient.fixture';
 import { ProfilListFixture } from 'src/app/manager/profil_list/profil-list.fixture';
+import { ProfilFixture } from 'src/app/manager/profil/profil.fixture';
 import { PatientListFixture } from 'src/app/manager/profil_list/patient/patient-list.fixture';
 import { CreationPatientComponent } from 'src/app/manager/creation_patient/page.component';
 import { InterfaceConfigurationFixture } from 'src/app/manager/interface-configuration/interface-configuration.fixture';
@@ -21,10 +22,19 @@ test.describe('Configuration Feature', () => {
         await expect(page).toHaveURL("http://localhost:4200/profil-list");
 
         await ProfilListFixture.getProfilButton(page);
-
+        
         await expect(page).toHaveURL("http://localhost:4200/profil");
 
-        
+        await page.locator("#chooseConfig").click();
+
+        await expect(page).toHaveURL("http://localhost:4200/configuration")
+
+        await page.locator("#addButton").click();
+
+        await expect(page).toHaveURL("http://localhost:4200/create-configuration")
+
+
+
 
 
     });
--- front-end/src/app/manager/profil/profil.fixture.ts
+++ front-end/src/app/manager/profil/profil.fixture.ts
@@ -1,5 +1,22 @@
 import { E2EComponentFixture } from "e2e/e2e-component.fixture";
+import { Page } from '@playwright/test';
 
 export class ProfilFixture extends E2EComponentFixture {
-    
+
+    // BUTTONS
+    getChangesInfosButtons() {
+        return this.page.getByTestId('changeInfos');
+    }
+
+    getChooseConfigButton() {
+        return this.page.getByTestId('chooseConfig');    
+    }
+
+    getResultsButton(){
+        return this.page.getByTestId('results');
+    }
+
+    clickChooseConfig(numberOfClick = 1){
+        return this.getChooseConfigButton().click({ clickCount: numberOfClick });
+    }
 }
