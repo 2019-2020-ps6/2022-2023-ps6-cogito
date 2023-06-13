d4e92c7fcc3dba1f64319637a680694b22841883
JulienS0t0 <juliensoto@free.fr>
2023-06-13 14:55:33 +0200

    feat: test play quiz 2q #161

--- front-end/e2e/scenarios/play-quiz.spec.ts
+++ front-end/e2e/scenarios/play-quiz.spec.ts
@@ -1,19 +1,18 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { patientPageUrl } from 'e2e/e2e.config';
-import { GamePageFixture } from 'src/app/game/page/game-page.fixture';
 
 // This file is here to test the playwright integration.
-test.describe('Play a quiz', () => {
+// test.describe('Play a quiz', () => {
 
-  test('Go on quiz',async ({page}) => {
-    await page.goto(patientPageUrl);
+//   test('Go on quiz',async ({page}) => {
+//     await page.goto(patientPageUrl);
 
-    await page.locator("app-patient-patient:first-child").click();
+//     await page.locator("app-patient-patient:first-child").click();
 
-    await page.locator("app-theme-theme:first-child").click();
+//     await page.locator("app-theme-theme:first-child").click();
 
-    await page.locator("app-quiz-quiz:first-child").click();
-  })
+//     await page.locator("app-quiz-quiz:first-child").click();
+//   })
 
   test('Play a quiz 2q with only good respond without correcting',async ({page}) => {
     await page.goto(patientPageUrl);
@@ -29,9 +28,10 @@ test.describe('Play a quiz', () => {
 
     await page.locator("button#true").click();
 
-    const resultElement = await page.$('quizresult h1');
+    const resultElement = await page.locator('quizresult .game-result-content h1');
     const resulText = await resultElement?.innerText();
 
+    expect(resulText).toBe('Félicitations vous avez fini le quiz !');
     
 
   })
