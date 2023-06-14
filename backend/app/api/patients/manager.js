2084bdf76030f9c5608c8dcfd3ea4e8cb019b8a4
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-06-14 22:22:45 +0200

    feature: add questions to quiz when asking quizzes of a patient #162

--- backend/app/api/patients/manager.js
+++ backend/app/api/patients/manager.js
@@ -1,6 +1,5 @@
 const { Patient, Quiz } = require('../../models')
-
-
+const {findQuizQuestions  } = require('../quizzes/questions/manager')
 
 
 /**
@@ -15,8 +14,15 @@ const getAllQuizzesOfPatient = (patientId) => {
     const quizzes = []
     for(let i = 0 ; i < themeIds.length; i++){
         const q = Quiz.get().filter((q) => q.themeId === themeIds[i]);
-        if(q != [])
+        if(q.length > 0){
+            console.log(q[0]);
+            console.log(findQuizQuestions(q[0].id))
+            q[0].questionList = findQuizQuestions(q[0].id);
+            for(let question of q[0].questionList){
+                delete question.quizId;
+            }
             quizzes.push(...q);
+        }
     }
     return quizzes
 }
